580d399cc205ee70e7c332494e0adbdc0fce9483
Pavel Schelkin <rhanan1233@gmail.com>
2025-02-04 16:52:36 +0300

    Тест ради теста

---
+++ src/test/java/faang/school/postservice/util/ExampleTest.java
@@ -0,0 +1,15 @@
+package faang.school.postservice.util;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(MockitoExtension.class)
+public class ExampleTest {
+    @Test
+    void test() {
+        fail("Тест ради теста");
+    }
+}
