0261670d021c8129be306f0c689274aa415e8d53
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-02-13 12:36:20 +0500

    fix

--- src/main/java/faang/school/postservice/broker/PubsubConfig.java
+++ src/main/java/faang/school/postservice/broker/PubsubConfig.java
@@ -3,7 +3,6 @@ package faang.school.postservice.broker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -37,10 +36,5 @@ public class PubsubConfig {
     @Bean
     public ChannelTopic likeEventTopic() {
         return new ChannelTopic(likeEventTopicName);
-    }
-
-    @Bean
-    MessagePublisher likeEventPublisher() {
-        return new LikeEventPublisher(redisTemplate(), likeEventTopic());
     }
 }
