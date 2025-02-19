ceb90984077cb47a548740cff5d5a23e3c66eaaa
Roman Shevchuk <reacount1393@gmail.com>
2025-02-20 02:07:14 +0300

    Замечание исправил, по крайней мере те что смог понять

--- src/main/java/faang/school/postservice/controller/like/LikeController.java
+++ src/main/java/faang/school/postservice/controller/like/LikeController.java
@@ -14,22 +14,22 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/likes")
+@RequestMapping("/posts")
 public class LikeController {
 
     private final LikeService likeService;
 
-    @GetMapping("/posts/{postId}")
+    @GetMapping("/posts/{postId}/users")
     public List<UserDto> getUsersWhoLikedPost(@PathVariable Long postId) {
-        log.info("Received request to get users who liked post with id: {}", postId);
+        log.debug("Received request to get users who liked post with id: {}", postId);
         List<UserDto> users = likeService.getUsersWhoLikedPost(postId);
-        log.info("Returned {} users who liked post with id: {}", users.size(), postId);
+        log.info("Received users {} who liked post with id: {}",users, postId);
         return users;
     }
 
-    @GetMapping("/comments/{commentId}")
+    @GetMapping("/comments/{commentId}/users")
     public List<UserDto> getUsersWhoLikedComment(@PathVariable Long commentId) {
-        log.info("Received request to get users who liked comment with id: {}", commentId);
+        log.debug("Received request to get users who liked comment with id: {}", commentId);
         List<UserDto> users = likeService.getUsersWhoLikedComment(commentId);
         log.info("Returned {} users who liked comment with id: {}", users.size(), commentId);
         return users;
