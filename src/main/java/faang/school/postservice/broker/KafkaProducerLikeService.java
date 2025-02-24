b4a1f1222fdb857f1bbaa135a249f50091d51c7e
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-02-24 20:06:38 +0500

    fix

--- src/main/java/faang/school/postservice/broker/KafkaProducerLikeService.java
+++ src/main/java/faang/school/postservice/broker/KafkaProducerLikeService.java
@@ -1,22 +1,32 @@
 package faang.school.postservice.broker;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.dto.like.LikePostEvent;
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class KafkaProducerLikeService {
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Value("${spring.kafka.like_post_event_topic_name}")
     private String likePostEventTopicName;
 
     public void sendLikePostEvent(LikePostEvent event) {
-        JSONObject json = new JSONObject(event);
+        String json;
+        try {
+            json = objectMapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            log.error("couldn't convert object to json", e);
+            throw new RuntimeException("couldn't convert object to json " + e.getMessage());
+        }
         kafkaTemplate.send(likePostEventTopicName, json.toString());
     }
 }
