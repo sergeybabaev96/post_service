d90f421ccfc7e2e2c9a3759f8048035e6adace63
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-02-24 15:07:10 +0500

    fix

--- src/main/java/faang/school/postservice/service/broker/KafkaProducerAlbumService.java
+++ src/main/java/faang/school/postservice/service/broker/KafkaProducerAlbumService.java
@@ -1,23 +1,33 @@
 package faang.school.postservice.service.broker;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import faang.school.postservice.dto.album.AlbumCreateEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class KafkaProducerAlbumService {
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Value("${spring.kafka.album-create-topic-name}")
     private String albumCreateTopicName;
 
     public void sendAlbumCreate(AlbumCreateEvent event) {
-        JSONObject jsonObject = new JSONObject(event);
-        String json = jsonObject.toString();
+        String json = null;
+        try {
+            json = objectMapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            log.error("couldn't convert object to json " + e);
+            throw new RuntimeException(e.getMessage());
+        }
         kafkaTemplate.send(albumCreateTopicName, json);
     }
 }
