99e811b2adca3ba33565560a06f658652c74afc9
Valerii Enin <enin_v_a@mail.ru>
2025-03-18 23:49:32 +0300

    BJS2-65243. Система постов.

---
+++ src/main/java/faang/school/postservice/mapper/PostMapper.java
@@ -0,0 +1,9 @@
+package faang.school.postservice.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public class PostMapper {
+
+}
