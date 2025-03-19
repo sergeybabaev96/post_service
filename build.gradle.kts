94236224ea0274cee6b051ba7c869b804144acf5
Ilyas Azhdar <azhdar902@gmail.com>
2025-03-19 10:26:09 +0500

    added coverage limit

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,8 +74,27 @@ tasks.bootJar {
 }
 
 tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
 }
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "CLASS"
+            includes = listOf("faang.school.postservice.service.*")
+
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = "0.50".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
