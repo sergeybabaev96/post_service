44fb9115886c5ba0c281ec061e6a57d4a5bfa665
Max Goloviznin <roxet59rus@mail.ru>
2025-03-19 19:28:58 +0500

    (BJS2-65278) fix failed test

--- src/test/java/faang/school/postservice/util/FailTest.java
+++ src/test/java/faang/school/postservice/util/FailTest.java
@@ -2,12 +2,9 @@ package faang.school.postservice.util;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class FailTest {
 
     @Test
     public void testFail() {
-        fail();
     }
 }
