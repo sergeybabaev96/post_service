6a2ad6af54e36d1cebc45f9956f6e40af1338b7d
Sergey <bigwave2915@gmail.com>
2025-03-20 23:12:08 +0500

    сделал commentMapper

---
+++ src/main/java/faang/school/postservice/dto/project/сomment/CommentDto.java
@@ -0,0 +1,11 @@
+package faang.school.postservice.dto.project.сomment;
+
+import lombok.Builder;
+
+@Builder
+public record CommentDto(
+        long postId,
+        String content,
+        long authorId
+) {
+}
