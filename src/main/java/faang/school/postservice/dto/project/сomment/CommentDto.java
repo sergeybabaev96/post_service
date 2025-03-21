ebbae7f1b5322d982c9b38d49926238d6640e6b1
Sergey <bigwave2915@gmail.com>
2025-03-21 11:45:04 +0500

    сделал CommentDto

--- src/main/java/faang/school/postservice/dto/project/сomment/CommentDto.java
+++ src/main/java/faang/school/postservice/dto/project/сomment/CommentDto.java
@@ -1,12 +1,24 @@
 package faang.school.postservice.dto.project.сomment;
 
-import lombok.Builder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Builder
-public record CommentDto(
-        long postId,
-        String content,
-        long authorId
+@Data
+@NoArgsConstructor
+public class CommentDto {
 
-) {
+    private Long id;
+
+    @NotBlank(message = "Content cannot be blank.")
+    @Size(max = 4096, message = "Content cannot exceed 4096 characters.")
+    private String content;
+
+    @NotNull(message = "Author ID cannot be null.")
+    private Long authorId;
+
+    @NotNull(message = "Post ID cannot be null.")
+    private Long postId;
 }
