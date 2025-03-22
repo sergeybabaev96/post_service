24983d487c1a548280095791b2a1d6cf4351dc05
alex198 <abykovskih@gmail.com>
2025-03-22 23:57:38 +0300

    BJS2-65283 (v1) добавлены методы в AlbumService, кроме избранных альбомов

--- src/main/java/faang/school/postservice/mapper/AlbumMapper.java
+++ src/main/java/faang/school/postservice/mapper/AlbumMapper.java
@@ -3,13 +3,11 @@ package faang.school.postservice.mapper;
 import faang.school.postservice.dto.album.AlbumDto;
 import faang.school.postservice.model.Album;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface AlbumMapper {
 
-    @Mapping(target = "", source = "")
     Album toAlbum(AlbumDto albumDto);
 
     AlbumDto toAlbumDto(Album album);
--- src/main/java/faang/school/postservice/repository/AlbumRepository.java
+++ src/main/java/faang/school/postservice/repository/AlbumRepository.java
@@ -4,7 +4,6 @@ import faang.school.postservice.model.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 
