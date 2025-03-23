26ec6d9ae49ca01e6824a39f8605d3102a22ed81
Anton <basanton@mail.ru>
2025-03-23 15:19:59 +0300

    v1

---
+++ src/main/java/faang/school/postservice/dto/like/LikeDto.java
@@ -0,0 +1,15 @@
+package faang.school.postservice.dto.like;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+public record LikeDto(Long userId,
+                      @JsonInclude(JsonInclude.Include.NON_NULL)
+                      Long postId,
+                      @JsonInclude(JsonInclude.Include.NON_NULL)
+                      Long commentId,
+                      @JsonInclude(JsonInclude.Include.NON_NULL)
+                      LocalDateTime timestamp) {
+
+}
