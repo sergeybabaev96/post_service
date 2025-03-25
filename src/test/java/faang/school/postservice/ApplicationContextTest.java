42e813c0f2c14c7ad549209bbbcb5d7b9d13538f
Dias Kamza <kamzadias31@gmail.com>
2025-03-25 20:41:30 +0500

    BJS2-53945 fix tests

--- src/test/java/faang/school/postservice/ApplicationContextTest.java
+++ src/test/java/faang/school/postservice/ApplicationContextTest.java
@@ -3,6 +3,8 @@ package faang.school.postservice;
 import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -13,24 +15,20 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 public class ApplicationContextTest {
 
+    @MockBean
+    private RedisTemplate<String, Long> redisTemplate;
+
     @Container
     static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:13.2")
             .withDatabaseName("test_db")
             .withUsername("test_user")
             .withPassword("test_password");
 
-    @Container
-    static final RedisContainer redisContainer = new RedisContainer(); // Использует настройки по умолчанию
-
-
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
-
-        registry.add("spring.data.redis.host", redisContainer::getHost);
-        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
     }
 
     @Test
