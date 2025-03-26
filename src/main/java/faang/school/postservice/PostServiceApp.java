3d93d4a0606501a919e785a4fc212f5e641232a7
KsushaTarasova <ksutaras543@mail.ru>
2025-03-26 13:48:16 +0300

    add enable retry

--- src/main/java/faang/school/postservice/PostServiceApp.java
+++ src/main/java/faang/school/postservice/PostServiceApp.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRetry
 @EnableFeignClients(basePackages = "faang.school.postservice.client")
 @EnableKafka
 public class PostServiceApp {
