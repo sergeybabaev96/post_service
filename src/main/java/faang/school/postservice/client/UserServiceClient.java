bc9c021dd3350ae4dae59996983fd6fb79ccd532
Lesha Kozyrev <alekseykoz@gmail.com>
2025-03-30 14:49:57 +0300

    Update feign clients.

--- src/main/java/faang/school/postservice/client/UserServiceClient.java
+++ src/main/java/faang/school/postservice/client/UserServiceClient.java
@@ -10,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
+//@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}")
+@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}${user-service.version}")
 public interface UserServiceClient {
 
-    @GetMapping("/api/v1/users/{userId}")
-    UserDto getUser(@PathVariable long userId);
+    @GetMapping("/users/{id}")
+    UserDto getUser(@PathVariable long id);
 
-    @PostMapping("/api/v1/users")
+    //@GetMapping("/api/v1/users/{userId}")
+    //UserDto getUser(@PathVariable long userId);
+
+    @PostMapping("/users")
     List<UserDto> getUsersByIds(@RequestBody List<Long> ids);
 
     @GetMapping("/{followerId}/followeeeids")
@@ -26,4 +30,14 @@ public interface UserServiceClient {
     @GetMapping("/subscription/{followeeId}/followerids")
     List<Long> getFollowerIdsByFolloweeId(
             @PathVariable @Min(value = 1L, message = "Followee id cannot be less than 1") long followeeId);
+/*
+@GetMapping("/api/v1/users/{userId}")
+    UserResponseDto getUser(@PathVariable long userId);
+
+    @GetMapping("/api/v1/subscriptions/followers/{followeeId}")
+    List<SubscriptionUserDto> getFollowers(@PathVariable long followeeId);
+
+    @PostMapping("/api/v1/users")
+    List<UserDto> getUsersByIds(@RequestBody List<Long> ids);
+ */
 }
