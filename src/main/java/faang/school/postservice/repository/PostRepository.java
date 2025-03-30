aae36299d88278d2543838cb1d34db5de0185012
Lesha Kozyrev <alekseykoz@gmail.com>
2025-03-30 14:17:42 +0300

    Changes were removed.

--- src/main/java/faang/school/postservice/repository/PostRepository.java
+++ src/main/java/faang/school/postservice/repository/PostRepository.java
@@ -1,13 +1,9 @@
 package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Post;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long>, JpaSpecificationExecutor<Post> {
@@ -24,32 +20,4 @@ public interface PostRepository extends CrudRepository<Post, Long>, JpaSpecifica
 
     @Query("SELECT p FROM Post p WHERE p.published = false AND p.deleted = false AND p.scheduledAt <= CURRENT_TIMESTAMP")
     List<Post> findReadyToPublish();
-
-    @Query(value = "SELECT * FROM post p " +
-            "WHERE (:authorIds IS NULL OR p.author_id IN (:authorIds)) " +
-            "AND p.deleted = false " +
-            "AND (:lastSeenDate IS NULL OR p.published_at < :lastSeenDate) " +
-            "ORDER BY p.published_at DESC " +
-            "LIMIT :quantity", nativeQuery = true)
-    List<Post> findPostsForFeed(@Param("authorIds") List<Long> authorIds,
-                                @Param("lastSeenDate") LocalDateTime lastSeenDate,
-                                @Param("quantity") int quantity);
-
-    /*
-    @Query(value = "SELECT * FROM post p " +
-            "WHERE p.author_id IN (:authorIds) " +
-            "AND p.deleted = false " +
-            "AND (:lastSeenDate IS NULL OR p.published_at < :lastSeenDate)" +
-            "ORDER BY p.published_at DESC " +
-            "LIMIT :quantity", nativeQuery = true)
-    List<Post> findPostsForFeed(List<Long> authorIds, LocalDateTime lastSeenDate, int quantity); */
-
-    /*@Query("SELECT p FROM Post p " +
-            "WHERE p.authorId IN :authorIds " +
-            "AND p.deleted = false " +
-            "AND (:lastSeenDate IS NULL OR p.publishedAt < :lastSeenDate) " +
-            "ORDER BY p.publishedAt DESC")
-    List<Post> findPostsForFeed(@Param("authorIds") List<Long> authorIds,
-                                @Param("lastSeenDate") LocalDateTime lastSeenDate,
-                                Pageable pageable); */
 }
