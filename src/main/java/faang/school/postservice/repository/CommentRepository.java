bbcf343c6f230538bfead1578c5e52978413729f
vtroshin <vtroshin@rvision.ru>
2025-04-02 18:40:30 +0300

    BJS2-66275 Добавил нужные методы для репозитория

--- src/main/java/faang/school/postservice/repository/CommentRepository.java
+++ src/main/java/faang/school/postservice/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.repository;
 
 import faang.school.postservice.model.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,4 +11,10 @@ public interface CommentRepository extends CrudRepository<Comment, Long> {
 
     @Query("SELECT c FROM Comment c WHERE c.post.id = :postId")
     List<Comment> findAllByPostId(long postId);
+
+    @Query("SELECT c FROM Comment c WHERE c.verifiedAt IS NULL")
+    List<Comment> getUnverifiedComments(Pageable pageable);
+
+    @Query(nativeQuery = true, value = "SELECT count(*) FROM comment c WHERE c.verified_at IS NULL")
+    int getUnverifiedCommentsCount();
 }
