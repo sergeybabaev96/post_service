40c9955879c2253cc085144608db2ff8d97e8140
vtroshin <vtroshin@rvision.ru>
2025-04-02 22:45:42 +0300

    BJS2-66275 Валидатор текстов

--- src/main/java/faang/school/postservice/service/ModerationDictionary.java
+++ src/main/java/faang/school/postservice/service/ModerationDictionary.java
@@ -1,11 +1,44 @@
 package faang.school.postservice.service;
 
+import faang.school.postservice.exception.ModerationDictionaryException;
+import jakarta.annotation.PostConstruct;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class ModerationDictionary {
 
-    public boolean isTextAreCorrect(String text) {
-        return Math.random() < 0.5;
+    private static final List<String> MODERATION_DICTIONARY_FILES = new ArrayList<>();
+
+    @Value("${dictionary.comments.path_to_file}")
+    private String pathToDictionary;
+
+    @PostConstruct
+    public void init() {
+        try (InputStream inputStream = getClass().getResourceAsStream(pathToDictionary)) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource not found: " + pathToDictionary);
+            }
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                MODERATION_DICTIONARY_FILES.addAll(reader.lines().map(String::toLowerCase).toList());
+            }
+        } catch (IOException e) {
+            throw new ModerationDictionaryException(e);
+        }
+    }
+
+    public boolean isTextAreCorrect(@NotBlank String text) {
+        return MODERATION_DICTIONARY_FILES.stream().noneMatch(text::contains);
     }
 }
