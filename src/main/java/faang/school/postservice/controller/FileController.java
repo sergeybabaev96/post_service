156445ba7a1f810f88ba616a63efe51dfd91c0f3
Mipusatina <mipspam@mail.ru>
2025-04-04 19:49:48 +0200

    FileController: max file size from application.yaml

--- src/main/java/faang/school/postservice/controller/FileController.java
+++ src/main/java/faang/school/postservice/controller/FileController.java
@@ -2,6 +2,7 @@ package faang.school.postservice.controller;
 
 import faang.school.postservice.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -25,11 +27,18 @@ import java.nio.file.Path;
 @RequiredArgsConstructor
 public class FileController {
 
+    @Value("${file.upload.max-size")
+    private int maxSize;
+
     private final FileService fileService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String uploadFile(@RequestBody MultipartFile file) {
+        if (file.getSize() > maxSize) {
+            throw new ResponseStatusException(HttpStatus.PAYLOAD_TOO_LARGE, "File is to big. Max size is " + maxSize);
+        }
+
         return fileService.uploadFile(file);
     }
 
