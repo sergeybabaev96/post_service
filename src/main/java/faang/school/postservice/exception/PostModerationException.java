4748c96bd21a2166dbc5bd4a136f1f7ee5eb4f9d
Yaroslav Olkin <shrelok.pupkin@yandex.ru>
2025-04-10 22:15:36 +0300

    исправил ошибки

--- src/main/java/faang/school/postservice/exception/PostModerationException.java
+++ src/main/java/faang/school/postservice/exception/PostModerationException.java
@@ -5,7 +5,11 @@ import faang.school.postservice.model.Post;
 import java.util.List;
 
 public class PostModerationException extends RuntimeException {
-    private final List<Post> failedPosts;
+    private List<Post> failedPosts;
+
+    public PostModerationException(String message) {
+        super(message);
+    }
 
     public PostModerationException(String message, List<Post> failedPosts) {
         super(message);
