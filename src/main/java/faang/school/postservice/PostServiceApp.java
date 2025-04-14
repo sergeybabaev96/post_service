a8bc80d8d431cc9d378aea281f7b68ef802ef632
KAS1990 <34664788+KAS1990@users.noreply.github.com>
2025-04-14 22:49:54 +0300

    fix: Removed unneeded annotation

--- src/main/java/faang/school/postservice/PostServiceApp.java
+++ src/main/java/faang/school/postservice/PostServiceApp.java
@@ -4,13 +4,11 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @EnableScheduling
 @EnableFeignClients(basePackages = "faang.school.postservice.client")
