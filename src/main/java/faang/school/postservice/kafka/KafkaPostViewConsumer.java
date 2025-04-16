46b002c3e9dc7d0e64f076e21f687ae68c4df9e3
Sergey Senin <04sersen@gmail.com>
2025-04-16 17:12:22 +0300

    Рефакторинг.

--- src/main/java/faang/school/postservice/kafka/KafkaPostViewConsumer.java
+++ src/main/java/faang/school/postservice/kafka/KafkaPostViewConsumer.java
@@ -5,6 +5,7 @@ import faang.school.postservice.service.RedisViewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -14,11 +15,14 @@ public class KafkaPostViewConsumer {
 
     private final RedisViewService redisViewService;
 
-    @KafkaListener(topics = "post_views", groupId = "postServiceListenerGroup")
-    public void consume(PostViewEvent event) {
+    @KafkaListener(
+            topics = "post_views",
+            groupId = "postServiceListenerGroup"
+    )
+    public void consume(PostViewEvent event, Acknowledgment acknowledgment) {
         try {
-            Long postId = event.getPostId();
-            redisViewService.incrementViewCount(postId);
+            redisViewService.incrementViewCount(event.getPostId());
+            acknowledgment.acknowledge();
         } catch (Exception e) {
             log.error("Failed to process PostViewEvent: {}", event, e);
         }
--- src/main/java/faang/school/postservice/service/RedisViewService.java
+++ src/main/java/faang/school/postservice/service/RedisViewService.java
@@ -17,19 +17,13 @@ public class RedisViewService {
 
     public void incrementViewCount(Long postId) {
         String key = POST_HASH_PREFIX + postId;
+        HashOperations<String, String, Object> hashOps = redisTemplate.opsForHash();
 
-        try {
-            HashOperations<String, String, Object> hashOps = redisTemplate.opsForHash();
-
-            if (Boolean.TRUE.equals(redisTemplate.hasKey(key))) {
-                hashOps.increment(key, "views", 1L);
-                log.info("View count incremented for post {}", postId);
-            } else {
-                log.warn("Post {} not found in Redis", postId);
-            }
-
-        } catch (Exception e) {
-            log.error("Error incrementing view count for post {}: {}", postId, e.getMessage());
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(key))) {
+            hashOps.increment(key, "views", 1L);
+            log.info("View count incremented for post {}", postId);
+        } else {
+            log.warn("Post {} not found in Redis", postId);
         }
     }
 }
