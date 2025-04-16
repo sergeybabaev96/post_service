327cc25834c0f2dfc6d23ecbdec7dd5e56022a91
Ilhan Mustafa <ilhnmst@gmail.com>
2025-04-16 15:20:39 +0300

    Refactor

--- src/main/java/faang/school/postservice/correcer/PostCorrecter.java
+++ src/main/java/faang/school/postservice/correcer/PostCorrecter.java
@@ -1,54 +1,28 @@
 package faang.school.postservice.correcer;
 
-import faang.school.postservice.client.speller.YandexSpellerClient;
-import faang.school.postservice.dto.post.PostDto;
-import faang.school.postservice.dto.speller.SpellerDto;
+import faang.school.postservice.service.post.PostCorrecterService;
 import faang.school.postservice.service.post.PostService;
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.List;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class PostCorrecter {
     private static final String CORRECTING_POSTS_FORM = "{} correcting the spelling of the posts";
-    private static final String CORRECTING_POST_FORM = "{} correct post {}";
 
-    private final YandexSpellerClient yandexSpellerClient;
     private final PostService postService;
+    private final PostCorrecterService postCorrecterService;
 
     @Async("postSpellingCorrecter")
     @Scheduled(cron = "${cron.correct_post}")
     public void correctingSpellingOfPosts() {
         log.info(CORRECTING_POSTS_FORM, "Start");
-        postService.getAllPosts().forEach(this::correctingSpellingPost);
+        postService.getAllPosts().forEach(postCorrecterService::correctingSpellingPost);
         log.info(CORRECTING_POSTS_FORM, "Finish");
     }
 
-    @Retryable(retryFor = FeignException.class, backoff = @Backoff(delay = 1000, multiplier = 2))
-    private void correctingSpellingPost(PostDto postDto) {
-        log.info(CORRECTING_POST_FORM, "Start", postDto);
-        StringBuilder builder = new StringBuilder(postDto.getContent());
-
-        List<SpellerDto> spellers = yandexSpellerClient.checkSpelling(postDto.getContent());
-        log.info("get spells {}", spellers);
-        Collections.reverse(spellers);
-        log.info("reverse spells {}", spellers);
-
-        spellers.forEach(speller ->
-                builder.replace(speller.getPos(), speller.getLen(), speller.getS().get(0)));
-
-        postDto.setContent(builder.toString());
-        postService.updatePost(postDto.getId(), postDto);
-        log.info(CORRECTING_POST_FORM, "Finish", postDto);
-    }
 }
