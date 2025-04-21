56cd33d0cf94f90f629dd1b7278bc2a281e3fa98
Hayotbek Farhodov <hayotbekfarhodov020@gmail.com>
2025-04-21 11:53:06 +0500

    LikePublisher

---
+++ src/main/java/faang/school/postservice/dto/event/LikeEvent.java
@@ -0,0 +1,21 @@
+package faang.school.postservice.dto.event;
+
+import faang.school.postservice.model.event.EventType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LikeEvent {
+    private long postId;
+    private long authorId;
+    private long userId;
+    private LocalDateTime likedAt;
+    private EventType type;
+}
